942cc1f7e943aa9c8e9e67ece03273165c84963c
jeremyrice98 <ricefamily1003@gmail.com>
2020-07-25 15:06:05 -0600

    update mysql

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE employee_trackerDB
+
+INSERT INTO department (id, name)
+VALUES 
+    (1, "Management"),
+    (2, "HR"),
+    (3, "Sales");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES 
+    (1, "Manager", 200000, 1),
+    (2, "Employee", 150000, 2);
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES 
+    (1, "Jeremy", "Rice", 1, Null),
+    (2, "Mckenzie", "Bergon", 2, 1);
